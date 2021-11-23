422485c508e815fde945700350946f6a5c26deb5
Gulbike Biyekenova <gbknva@gmail.com>
2021-11-23 21:35:47 +0000

    Queries 4

---
+++ 4_queries/27_name_of_teachers_assisted.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT cohorts.name as cohort, teachers.name as teacher
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
+ORDER BY teacher;
